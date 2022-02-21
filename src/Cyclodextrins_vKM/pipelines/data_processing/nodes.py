e77e11a89f65c71609ae45459628d6bb83a13771
MAIHfly <mhosfeld@andrew.cmu.edu>
2022-02-21 08:48:52 -0500

    add comments to nodes.py

--- src/Cyclodextrins_vKM/pipelines/data_processing/nodes.py
+++ src/Cyclodextrins_vKM/pipelines/data_processing/nodes.py
@@ -4,36 +4,36 @@ orca = ade.methods.ORCA()
 import numpy as np
 
 def ConvertMol(x):
-    Solvent = input('If you got a solvent give us the name here if not just hit enter: ')
-    Name = input('Give a name for the file ending in .xyz: ')
-    Charge = input('Specify charge of the molecule: ')
-    Multiplicity = input('Specify multiplicity of molecule (if not given it will be 1): ')
+    Solvent = input('If you got a solvent give us the name here if not just hit enter: ') # configure Solvent
+    Name = input('Give a name for the file ending in .xyz: ') # configure name of .xyz file
+    Charge = input('Specify charge of the molecule: ') # configure charge
+    Multiplicity = input('Specify multiplicity of molecule (if not given it will be 1): ') # configure multiplicity
 
     if Multiplicity == '':
         Multiplicity = 1
-
+    # make sure that the inputs that need to be integers are integers
     Charge = int(Charge)
     Multiplicity = int(Multiplicity)
-
+    # adjust solvent 
     if Solvent == '':
         Solvent = None
-    
+    # decode input file from utf-8 to string
     xnew = x.decode("utf-8")
-
+    # open and write .xyz file with the decoded input
     with open(Name,'w') as XYZfile:
         XYZfile.write(xnew)
-
+    # create molecule
     MoI = ade.Molecule(Name, solvent_name=Solvent, charge=Charge, mult=Multiplicity)
-
+    # close .xyz file
     XYZfile.close()
 
     return(MoI)
-
+# optimize the molecular structure method can be changed
 def OptimizeMol(MoI):
     print(MoI)
     MoI.optimise(method=ade.methods.XTB())
     return(MoI)
-
+# Run Calculation, specify the number of cores for the calculation, and output calculation to files
 def CalculateMol(MoI):
     NoC = input('How many cores do you have/want to use if you only have 1 hit enter: ')
     if NoC == '':
@@ -41,7 +41,7 @@ def CalculateMol(MoI):
     CoI = ade.Calculation(name=MoI.name,molecule=MoI,method=orca,keywords=orca.keywords.hess,n_cores=NoC)
     CoI.output.filename = MoI.name+'.out'
     return(CoI)
-
+# get Gibbs free energy using the calc_thermo property
 def GetGibbsMol(MoI,CoI):
     NoC = input('How many cores do you have/want to use if you only have 1 hit enter: ')
     if NoC == '':
@@ -50,12 +50,12 @@ def GetGibbsMol(MoI,CoI):
     print(f'G = {MoI.free_energy:.6f} Ha')
     GibbsE = MoI.free_energy
     return(GibbsE)
-
+# get delta gibbs free energy
 def GetDeltGibbs(GibbsE1, GibbsE2):
     DeltGibbs = np.abs(GibbsE1 - GibbsE2)
     print(DeltGibbs)
     return(DeltGibbs)
-
+# get pKa (there is an error here so result calculation will be done manually)
 def GetPka(DeltGibbsE):
     DeltGibbsEkJpM = 2600*DeltGibbsE
     R = 0.008314
